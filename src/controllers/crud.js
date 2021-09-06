2fcb76ec15e6d31716f52efea423f5afc3e92f8f
leandrojg <79561670+leandrojg@users.noreply.github.com>
2021-09-06 17:03:08 -0300

    Fix listarID

--- src/controllers/crud.js
+++ src/controllers/crud.js
@@ -1,83 +1,66 @@
-
 let productos = [];
 
 exports.saveData = (req, res, next) => {
+  let { title, price, thumbnail } = req.body;
 
-    let {title, price, thumbnail} = req.body
+  const producto = req.body;
+  const id = productos.length + 1;
 
-    const producto = req.body;
-    const id = productos.length + 1;
+  const newProduct = {
+    id: id,
+    title: producto.title,
+    price: producto.price,
+    thumbnail: producto.thumbnail,
+  };
 
-    const newProduct = {
-      id: id,
-      title: producto.title,
-      price: producto.price,
-      thumbnail: producto.thumbnail,
-    }
+  productos = [...productos, newProduct];
+  console.log(productos);
 
-    productos = [...productos, newProduct]
-    console.log(productos)
-
-
-    res.send(title + " " + price + " " + thumbnail)
-  }
-  
+  res.send(title + " " + price + " " + thumbnail);
+};
 
 exports.showData = (req, res, next) => {
-  res.render("index",
-    {   
-        lista: productos,
-        mensaje: "Mensaje desde showData crud!",
-        min: 3,
-        max: 20,
-        status: true
-
-    });
-  }
-
+  res.render("index", {
+    lista: productos,
+    mensaje: "Mensaje desde showData crud!",
+    min: 3,
+    max: 20,
+    status: true,
+  });
+};
 
 exports.showForID = (req, res, next) => {
-
-      if(!isNaN(req.params.id)){
-        let id = parseInt(req.params.id,10);
-        if(id >= 0 && id <= productos.length-1){
-        const producto = productos.find(producto => producto.id === id);
-        res.send(producto);
-        }else{
-        res.send('Fuera de rango...')
-        }
-        }else
-        res.send('Esto no es un numero')
+  if (!isNaN(req.params.id)) {
+    let id = parseInt(req.params.id, 10);
+    if (id > 0 && id <= productos.length) {
+      const producto = productos.find((producto) => producto.id === id);
+      res.send(producto);
+    } else {
+      res.send("Fuera de rango...");
     }
-  
-
+  } else res.send("Esto no es un numero");
+};
 
 exports.deleteId = (req, res, next) => {
-      
-          const id = parseInt(req.params.id,10);
-          productos = productos.filter(produ => produ.id !== id);
-          res.json(productos) 
-      }
-    
-
-
+  const id = parseInt(req.params.id, 10);
+  productos = productos.filter((produ) => produ.id !== id);
+  res.json(productos);
+};
 
 exports.upDateId = (req, res, next) => {
-      
-  const id = parseInt(req.params.id,10);
-  const {title,price, thumbnail} = req.body
+  const id = parseInt(req.params.id, 10);
+  const { title, price, thumbnail } = req.body;
 
   if (title && price && thumbnail) {
-      productos.forEach((producto, i) => {
-          if(producto.id === id) {
-              producto.title = title;
-              producto.price = price;
-              producto.thumbnail = thumbnail;
-          }
-      }) ;
-      res.json(productos);
-  }else{
-      res.status(500).json({error: 'Todos los campos son requeridos...'})
+    productos.forEach((producto, i) => {
+      if (producto.id === id) {
+        producto.title = title;
+        producto.price = price;
+        producto.thumbnail = thumbnail;
+      }
+    });
+    res.json(productos);
+  } else {
+    res.status(500).json({ error: "Todos los campos son requeridos..." });
   }
-}
-
+};
